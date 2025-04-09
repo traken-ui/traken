95e40094532e801e3a21f91b9a82d397a38f46c4
Ali Raza Jafri <jafriali377@gmail.com>
2025-04-09 14:29:26 +0530

    complete input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,35 +1,65 @@
 import { cn } from "../../lib/utils";
-import { inputVariants } from "./inputVariants";
+import { inputVariants,labelVariants } from "./inputVariants";
 import React, { InputHTMLAttributes } from "react";
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+type NativeInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, "size">;
+export interface InputProps extends NativeInputProps {
     className?: string;
+    fullWidth?: boolean;
+    disabled?: boolean;
+    inputSize?: number;
     color?: "base" | "primary" | "secondary" | "success" | "warning" | "danger";
     rounded?: "sm" | "md" | "lg" | "xl" | "full";
-    variant?: "default" | "disabled";
+    size?: "base"|"sm" | "md" | "lg" | "xl";
+    variant?: "flat" | "faded" | "bordered" | "underlined";
+    label?:string;
+    labelPlacement?: "top"|"left"
 }
 
 const Input = ({
+    placeholder,
+    id,
+    label,
+    labelPlacement,
+    size,
+    fullWidth,
     className,
     variant,
     color,
     rounded,
+    disabled,
+    inputSize,
     ...props
 }: InputProps) => {
     const inputRef = React.useRef<HTMLInputElement>(null);
     return (
-        <input
-            className={cn(
-                inputVariants({
-                    color,
-                    rounded,
-                    variant,
-                }),
-                className,
-            )}
-            ref={inputRef}
-            {...props}
-        />
+        <div className="relative z-10 bg-transparent">
+            <label htmlFor={id} className={cn(labelVariants({
+                labelPlacement,
+                color,
+                size,
+            }))}>
+                {label}
+            </label>
+            <input
+                placeholder={placeholder}
+                id={id}
+                size={inputSize}
+                disabled={disabled}
+                className={cn(
+                    inputVariants({
+                        size,
+                        fullWidth,
+                        color,
+                        rounded,
+                        variant,
+                    }),
+                    className,
+                )}
+                ref={inputRef}
+                {...props}
+            />
+        </div>
     );
 };
 Input.displayName = "Input";
