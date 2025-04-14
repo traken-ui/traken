ccf19d5da272dc6f906fa4384e31d9f4a8a731bd
sirajshaikh-dev <ss1425306@gmail.com>
2025-04-14 19:01:19 +0530

    Fix import paths

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 import { calendarVariants } from "./CalendarVariants";
 import { CalendarHeader } from "./CalendarHeader";
 import { CalendarGrid } from "./CalendarGrid";
