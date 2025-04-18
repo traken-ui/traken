299d86343599cc0b9a32039849485feb60a94bd6
Aquib Jawed <aquibjawed.dev@gmail.com>
2025-04-18 15:41:50 +0530

    Switch fixed

--- src/components/switch/Switchvariants.tsx
+++ src/components/switch/Switchvariants.tsx
@@ -8,45 +8,32 @@ export const toggleVariants = cva(
         sm: "w-10 h-5",
         md: "w-12 h-6",
         lg: "w-14 h-7",
-        xl : "w-16 h-8",
-        
+        xl: "w-16 h-8",
       },
       checked: {
         true: "bg-blue-500",
         false: "bg-gray-300",
       },
-      color:{
-
-        base : "bg-gray-500",
-        primary:"bg-blue-500",
-        secondary:"bg-purple-500",
-        success:"bg-green-500",
-        warning:"bg-yellow-500",
-        danger:"bg-red-500",
-        transperent:"bg-transparent"
-
-      }
-
+      color: {
+        base: "bg-base",
+        primary: "bg-primary",
+        secondary: "bg-secondary",
+        success: "bg-success",
+        warning: "bg-warning",
+        danger: "bg-danger",
+        transperent: "bg-transparent",
+      },
     },
-    compoundVariants:[
-
-        {checked:false, color:"base", className:"bg-gray-500 dark:bg-gray-700"},
-        {checked:false, color:"primary", className:"bg-blue-500 dark:bg-gray-700"},
-        {checked:false, color:"secondary", className:"bg-purple-500 dark:bg-gray-700"},
-        {checked:false, color:"success", className:"bg-green-500 dark:bg-gray-700"},
-        {checked:false, color:"warning", className:"bg-yellow-500 dark:bg-gray-700"},
-        {checked:false, color:"danger", className:"bg-red-500 dark:bg-gray-700"},
-        {checked:false, color:"transperent", className:"bg-transparent border border-red-500 dark:border-red-300"},
-        {checked:true, color:"transperent", className:"bg-transparent border border-green-500 dark:border-green-300"}
-
+    compoundVariants: [
+      {
+        checked: false,
+        className: "bg-transparent border border-gray-600 dark:border-gray-300",
+      },
     ],
     defaultVariants: {
       size: "md",
-      checked: false,
-      color:"primary"
+      checked: true,
+      color: "primary",
     },
-  }
+  },
 );
-
-
-{/* <div className="group peer ring-0 bg-gradient-to-r from-rose-400 to-red-900  rounded-full outline-none duration-700 after:duration-300 w-24 h-12  shadow-md peer-checked:bg-gradient-to-r peer-checked:from-emerald-500 peer-checked:to-emerald-900 peer-focus:outline-none  after:content-[''] after:rounded-full after:absolute after:bg-gray-50 after:outline-none after:h-10 after:w-10 after:top-1 after:left-1  peer-checked:after:translate-x-12 peer-hover:after:scale-95"> */}
